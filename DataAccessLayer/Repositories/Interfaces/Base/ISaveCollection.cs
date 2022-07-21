83c0fc206f3d2493773dd6da3f85199003e1ea30
a.sutormin <a.sutormin@muiz.ru>
2022-07-21 18:28:43 +0300

    Интерфейс для сохранения через DataTable.

---
+++ DataAccessLayer/Repositories/Interfaces/Base/ISaveCollection.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Base;
+
+namespace DataAccessLayer.Repositories.Interfaces.Base
+{
+    /// <summary>
+    /// Интерфейс для быстрого сохранения коллекции объектов.
+    /// </summary>
+    /// <typeparam name="T">Тип объекта в коллекции.</typeparam>
+    public interface ISaveCollection<T> where T : BaseEntity
+    {
+        /// <summary>
+        /// Сохраняет коллекцию объектов.
+        /// </summary>
+        /// <param name="collection"></param>
+        void SaveCollection(IEntityCollection<T> collection);
+    }
+}
